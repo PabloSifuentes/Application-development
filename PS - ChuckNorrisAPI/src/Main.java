0886d2d521afda0501f054561df1171ead476c82
PABLO SIFUENTES <pablo_sifuentes@estudante.sesisenai.org.br>
2024-02-26 21:44:39 -0300

    Método de tradução adicionado

--- PS - ChuckNorrisAPI/src/Main.java
+++ PS - ChuckNorrisAPI/src/Main.java
@@ -3,10 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         try { // URL da API do Chuck Norris
             String apiUrl = "https://api.chucknorris.io/jokes/random";
 
@@ -23,8 +24,13 @@ public class Main {
                 resposta.append(linha);
             }
             leitor.close();
+
             String piada = obterPiadaChuchNorris(resposta.toString());
             System.out.println(piada);
+
+            String piadaTraduzida = traduzirParaPortuguese(piada);
+            System.out.println(piadaTraduzida);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -44,4 +50,39 @@ public class Main {
         int fimDoIndice = resposta.lastIndexOf("\"");
         return resposta.substring(inicioDoIndice, fimDoIndice);
     }
+    /**
+     * Método para traduzir a piada para o português usando a API do Google Translate.
+     * @param piadaParaTraduzir A piada a ser traduzida.
+     * @return A piada traduzida para o português.
+     */
+    private static String traduzirParaPortuguese(String piadaParaTraduzir) {
+        try {
+            // URL da API do Google Translate
+            String apiUrl = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=pt&dt=t&q=";
+
+            // Fazendo a requisição GET para a API do Google Translate
+            HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl + URLEncoder.encode(piadaParaTraduzir, "UTF-8")).openConnection();
+            connection.setRequestMethod("GET");
+
+            // Lendo a resposta da API
+            BufferedReader leitor = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder resposta = new StringBuilder();
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                resposta.append(linha);
+            }
+            leitor.close();
+
+
+            // Extraindo a tradução do JSON retornado pela API
+            String traducao = resposta.substring(5, resposta.indexOf("\",\""));
+
+            // Retornando a tradução
+            return traducao;
+
+        } catch (IOException e) {
+            return "Erro ao traduzir a piada para o português.";
+        }
+    }
+
 }
