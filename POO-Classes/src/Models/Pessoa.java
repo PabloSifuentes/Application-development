f34f09831f45b69e66a3a2fff06321b1e0c2edec
PABLO SIFUENTES <pablo_sifuentes@estudante.sesisenai.org.br>
2024-04-03 22:01:41 -0300

    Aula Classes

---
+++ POO-Classes/src/Models/Pessoa.java
@@ -0,0 +1,16 @@
+package Models;
+
+public class Pessoa {
+
+    public String nome = "Pablo";
+
+    public int idade = 19;
+
+    public double altura = 1.75;
+
+    //Método
+    public void falar() {
+        System.out.println("Olá, meu nome é" + nome + " e eu tenho" + idade + "anos.");
+    }
+ }
+
